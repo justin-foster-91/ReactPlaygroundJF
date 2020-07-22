edc87036e379705de0f14ee9aa4fce388f0f0ae0
Justin Foster <jfoster91@gmail.com>
2020-07-22 11:52:17 -0400

    Initial upload

---
+++ src/state-drills/drills.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Drills extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {World: "who"};
+  }
+
+  render() {
+    return (
+      <main className='Drills'>
+        <p>Hello World</p>
+        <button 
+          variant="outline-dark"
+          onClick={function() {console.log('clicked!')}}
+        >World</button>
+        <button variant="outline-dark">Friend</button>
+        <button variant="outline-dark">React</button>
+      </main>
+    );
+  }
+}
+
+export default Drills;
